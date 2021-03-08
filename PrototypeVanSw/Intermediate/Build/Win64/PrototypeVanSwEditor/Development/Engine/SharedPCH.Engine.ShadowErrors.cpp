35ab431c3460a83f021a4d312576db228675a898
Daniel Rueda <danruedalop@gmail.com>
2021-03-08 15:21:18 +0100

    upload c++ project v4.26.1

---
+++ PrototypeVanSw/Intermediate/Build/Win64/PrototypeVanSwEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29910
+#include "C:/Users/danru/Desktop/RepoPrototipoTFM/PrototipoVanishingSword/PrototypeVanSw/Intermediate/Build/Win64/PrototypeVanSwEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ PrototypeVanSw/Intermediate/Build/Win64/UE4Editor/Inc/PrototypeVanSw/PrototypeVanSwCharacter.generated.h
@@ -0,0 +1,97 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PROTOTYPEVANSW_PrototypeVanSwCharacter_generated_h
+#error "PrototypeVanSwCharacter.generated.h already included, missing '#pragma once' in PrototypeVanSwCharacter.h"
+#endif
+#define PROTOTYPEVANSW_PrototypeVanSwCharacter_generated_h
+
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_SPARSE_DATA
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_RPC_WRAPPERS
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPrototypeVanSwCharacter(); \
+	friend struct Z_Construct_UClass_APrototypeVanSwCharacter_Statics; \
+public: \
+	DECLARE_CLASS(APrototypeVanSwCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/PrototypeVanSw"), NO_API) \
+	DECLARE_SERIALIZER(APrototypeVanSwCharacter)
+
+
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAPrototypeVanSwCharacter(); \
+	friend struct Z_Construct_UClass_APrototypeVanSwCharacter_Statics; \
+public: \
+	DECLARE_CLASS(APrototypeVanSwCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/PrototypeVanSw"), NO_API) \
+	DECLARE_SERIALIZER(APrototypeVanSwCharacter)
+
+
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APrototypeVanSwCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APrototypeVanSwCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APrototypeVanSwCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APrototypeVanSwCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APrototypeVanSwCharacter(APrototypeVanSwCharacter&&); \
+	NO_API APrototypeVanSwCharacter(const APrototypeVanSwCharacter&); \
+public:
+
+
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APrototypeVanSwCharacter(APrototypeVanSwCharacter&&); \
+	NO_API APrototypeVanSwCharacter(const APrototypeVanSwCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APrototypeVanSwCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APrototypeVanSwCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(APrototypeVanSwCharacter)
+
+
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(APrototypeVanSwCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(APrototypeVanSwCharacter, FollowCamera); }
+
+
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_9_PROLOG
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_SPARSE_DATA \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_RPC_WRAPPERS \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_INCLASS \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_SPARSE_DATA \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PROTOTYPEVANSW_API UClass* StaticClass<class APrototypeVanSwCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID PrototypeVanSw_Source_PrototypeVanSw_PrototypeVanSwCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
