121afb5a07f652e1562986be548201ac017c1531
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2021-03-29 23:06:44 +0200

    fixed Default Property Warning ThirdPersonCharacter_C

--- PrototypeVanSw/Source/PrototypeVanSw/PrototypeVanSwGameMode.cpp
+++ PrototypeVanSw/Source/PrototypeVanSw/PrototypeVanSwGameMode.cpp
@@ -7,7 +7,7 @@
 APrototypeVanSwGameMode::APrototypeVanSwGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Code/Player/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
